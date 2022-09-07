ee298f011e9ada545e8d499ba8abe461f77a1fb1
Dennea Maccallum <denneamaccallum@Denneas-MacBook-Air.local>
2022-09-06 20:05:54 -0600

    Add background,search bar and Weather API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
+const api = {
+  key: "69fa7e908b97f1a77b798a64bd047f36",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+           type = "text"
+           className = "search-bar"
+           placeholder = "Search for a city..."
+           />
+        </div>
+      </main>
     </div>
   );
 }
